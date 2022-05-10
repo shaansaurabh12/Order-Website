f2fe1fe0fc12ec38f3fa4f5038ca13253998ad76
Saurabh Joshi <72164136+shaansaurabh12@users.noreply.github.com>
2022-05-11 00:59:46 +0530

    Add files via upload
    This file will edit as per the demand.

---
+++ 2ood/index.php
@@ -0,0 +1,38 @@
+<HTML>
+<HEAD>
+<TITLE>Welcome to 24X7 Foodies.com!</TITLE>
+<style type="text/css">
+@import url(style.css);
+   a:link {color: #ffffff}
+   a:visited {color: #ffffff}
+   a:hover {color: #ffffff}
+   a:active {color: #ffffff}
+</style>
+</HEAD>
+<BODY background="background1.jpg">
+<?php include("header.php"); ?>
+<FONT size="5" color="white">
+<SECTION align="center"><A HREF="index.php"><IMG SRC="logo.png" alt="Home" id="logo"></IMG></A></SECTION>
+<SECTION>
+<MAIN>
+<B><P>We are available only at the following regions.<BR> Please select any one:</P></B></FONT>
+<SECTION align="center"><IMG src="clickhere.gif" width="100" height="50"></IMG></SECTION>
+<TABLE >
+<TR><TD><FONT size="6"  color="white">
+<A HREF="order.php" style="text-decoration: none">Bareilly</A></FONT></TD></TR>
+<TR><TD><FONT size="6"  color="white">
+<A HREF="order.php" style="text-decoration: none">Noida</A></FONT></TD></TR>
+<TR><TD><FONT size="6"  color="white">
+<A HREF="order.php" style="text-decoration: none">Gurugram</A></FONT></TD></TR>
+<TR><TD><FONT size="6"  color="white">
+<A HREF="order.php" style="text-decoration: none">Mumbai</A></FONT></TD></TR>
+</TABLE>
+</SECTION>
+</MAIN><BR><HR width="1000">
+<FOOTER >
+<FONT size="5" color="white" background-color: #fefbd8>
+</FONT>
+<h1 style="background-color: #fefbd8">This Website Created By Saurabh Joshi. <br> Copyright © 2022, Personal Use Only Not For Commercial Purpose </h1>
+</FOOTER>
+</BODY>
+</HTML>
